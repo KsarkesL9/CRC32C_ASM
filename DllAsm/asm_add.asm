049ee8e32e6f9286e9fe70c73af7ca2a7f3f254b
michalsadkowski4 <michalsadkowski@hotmail.com>
2025-12-18 18:49:13 +0100

    feat: Implement initial CRC32C GUI application with various algorithm options, multi-threading, and memory management features.

--- DllAsm/asm_add.asm
+++ DllAsm/asm_add.asm
@@ -1,13 +1,22 @@
-.DATA
+.data
 align 16
-k1k2 QWORD 0740EEF02h, 09E4ADDF8h
-k3k4 QWORD 0F20C0DFEh, 0493C7D27h
-k5k6 QWORD 03DA6D0CBh, 0BA4FC28Eh
-kCk0 QWORD 0F48642E9h, 000000000h
+; CRC32C constants (polynomial 0x82F63B78)
+CONST_K1K2      DQ 00000000ccaa009eh, 00000001751997d0h   ; K1=x^544 mod P, K2=x^480 mod P
+CONST_K3K4      DQ 0163cd6124h, 00000001f7011640h        ; K3=x^160 mod P, K4=x^96 mod P
+CONST_K5K6      DQ 0579665f3h, 047b04d9bh
+CONST_BARRETT   DQ 0dea713f1h, 000000000h
+CONST_POLY      DQ 082f63b78h, 000000000h
+CONST_MASK      DD 000000000h, 000000000h, 0ffffffffh, 0ffffffffh
 kAkB QWORD 0155AD968h, 02E7D11A7h
 kAkB_3way QWORD 08A074012h, 093E106A4h
-k_pct QWORD 0DDA0442Bh, 0DEBB642Fh
-kPoly QWORD 011EDC6F41h, 011EDC6F41h 
+; Stałe dla 1024-bajtowego stride (x^16384 mod P, x^8192 mod P)
+kAkB_1024 QWORD 0F20C0DFEh, 0493C7D27h
+
+
+align 16
+Constants_K1K2      DQ 00000000ccaa009eh, 00000001751997d0h
+Constants_K3K4      DQ 00000001f7860f71h, 0000000163cd6124h
+Constants_MuPoly    DQ 0000000dea713f23h, 0000000105ec76f0h
 
 .CODE
 
@@ -101,178 +110,216 @@ exit_func:
     ret
 AsmCrc32cUpdate3Way ENDP
 
-AsmCrc32cUpdateFusion PROC
+
+
+
+AsmCrc32cUpdateFusion PROC FRAME
     push rbx
+    .pushreg rbx
     push rdi
+    .pushreg rdi
     push rsi
-    sub rsp, 16 + 16 
+    .pushreg rsi
+    .endprolog
 
-    mov eax, ecx        
-    mov rdi, rdx     
-    mov rsi, r8        
+    mov eax, ecx
+    mov rdi, rdx       
+    mov rsi, r8         
 
-  
-    cmp rsi, 256
-    jb use_legacy
+    ; Użyj ścieżki skalarnej dla wszystkich rozmiarów (vector path wymaga naprawy)
+    jmp scalar_fallback
 
-  
-    mov r8, rdi
-    and r8, 15
-    jz aligned_start
-    mov rcx, 16
-    sub rcx, r8        
-    sub rsi, rcx       
-    
+    test rdi, 15
+    jz vector_init
+
 align_loop:
     crc32 eax, byte ptr [rdi]
     inc rdi
-    dec rcx
+    dec rsi
+    test rdi, 15
     jnz align_loop
 
-aligned_start:
-   
-    movd xmm0, eax      
-    
- 
-    movdqa xmm10, XMMWORD PTR [k1k2]
-    movdqa xmm11, XMMWORD PTR [k3k4]
-    
-   
-    movdqa xmm1, [rdi]
-    movdqa xmm2, [rdi+16]
-    movdqa xmm3, [rdi+32]
-    movdqa xmm4, [rdi+48]
-    
-   
-    pxor xmm1, xmm0
-    
+vector_init:
+    movdqa xmm10, xmmword ptr [CONST_K1K2]
+    movdqa xmm11, xmmword ptr [CONST_K3K4]
+
+    movd xmm0, eax
+    pxor xmm0, xmmword ptr [rdi]
+    movdqa xmm1, xmmword ptr [rdi + 16]
+    movdqa xmm2, xmmword ptr [rdi + 32]
+    movdqa xmm3, xmmword ptr [rdi + 48]
     add rdi, 64
     sub rsi, 64
 
-   
+vector_loop:
     cmp rsi, 64
-    jb fold_final_64
+    jb vector_fold
 
-align 16
-fold_loop:
-   
-    prefetcht0 [rdi + 256]
-
-   
-    
-  
-    movdqa xmm5, xmm1
-    movdqa xmm6, xmm2
-    
-    pclmulqdq xmm1, xmm10, 11h 
-    pclmulqdq xmm5, xmm10, 00h 
-    
-    pclmulqdq xmm2, xmm10, 11h
-    pclmulqdq xmm6, xmm10, 00h
-    
-    pxor xmm1, xmm5
-    pxor xmm2, xmm6
-    
-    pxor xmm1, [rdi]      
-    pxor xmm2, [rdi+16]
-
-   
-   
-    movdqa xmm7, xmm3
-    movdqa xmm8, xmm4
-    
-    pclmulqdq xmm3, xmm10, 11h
-    pclmulqdq xmm7, xmm10, 00h
-    
+    ; Interleaved Folding (4x128 bit)
+    movdqa xmm4, xmm0
     pclmulqdq xmm4, xmm10, 11h
-    pclmulqdq xmm8, xmm10, 00h
-    
-    pxor xmm3, xmm7
-    pxor xmm4, xmm8
-    
-    pxor xmm3, [rdi+32]
-    pxor xmm4, [rdi+48]
-    
+    pclmulqdq xmm0, xmm10, 00h
+    pxor xmm0, xmm4
+    pxor xmm0, xmmword ptr [rdi]
+
+    movdqa xmm5, xmm1
+    pclmulqdq xmm5, xmm10, 11h
+    pclmulqdq xmm1, xmm10, 00h
+    pxor xmm1, xmm5
+    pxor xmm1, xmmword ptr [rdi + 16]
+
+    movdqa xmm4, xmm2
+    pclmulqdq xmm4, xmm10, 11h
+    pclmulqdq xmm2, xmm10, 00h
+    pxor xmm2, xmm4
+    pxor xmm2, xmmword ptr [rdi + 32]
+
+    movdqa xmm5, xmm3
+    pclmulqdq xmm5, xmm10, 11h
+    pclmulqdq xmm3, xmm10, 00h
+    pxor xmm3, xmm5
+    pxor xmm3, xmmword ptr [rdi + 48]
+
     add rdi, 64
     sub rsi, 64
-    cmp rsi, 64
-    jae fold_loop
+    jmp vector_loop
 
-fold_final_64:
-    
-    
-   
-    movdqa xmm5, xmm1
-    pclmulqdq xmm1, xmm11, 11h ; High * K3
-    pclmulqdq xmm5, xmm11, 00h ; Low * K4
-    pxor xmm1, xmm5
-    pxor xmm1, xmm2
-    
-   
-    movdqa xmm5, xmm1
-    pclmulqdq xmm1, xmm11, 11h
-    pclmulqdq xmm5, xmm11, 00h
-    pxor xmm1, xmm5
-    pxor xmm1, xmm3
+vector_fold:
+    movdqa xmm4, xmm0
+    pclmulqdq xmm4, xmm11, 11h
+    pclmulqdq xmm0, xmm11, 00h
+    pxor xmm0, xmm4
+    pxor xmm0, xmm1
 
-  
-    movdqa xmm5, xmm1
-    pclmulqdq xmm1, xmm11, 11h
-    pclmulqdq xmm5, xmm11, 00h
-    pxor xmm1, xmm5
-    pxor xmm1, xmm4
+    movdqa xmm4, xmm0
+    pclmulqdq xmm4, xmm11, 11h
+    pclmulqdq xmm0, xmm11, 00h
+    pxor xmm0, xmm4
+    pxor xmm0, xmm2
 
-   
-    
-    movdqa xmm12, XMMWORD PTR [k5k6]
-    
-   
-    movdqa xmm2, xmm1
-    pclmulqdq xmm1, xmm12, 11h 
-    pclmulqdq xmm2, xmm12, 00h 
-    pxor xmm1, xmm2
-    
-   
-    
-    jmp use_legacy
+    movdqa xmm4, xmm0
+    pclmulqdq xmm4, xmm11, 11h
+    pclmulqdq xmm0, xmm11, 00h
+    pxor xmm0, xmm4
+    pxor xmm0, xmm3
 
-   
+    ; Konwersja stanu XMM na CRC32C przez instrukcje sprzętowe
+    ; Zapisz xmm0 na stos i przelicz przez crc32
+    sub rsp, 16
+    movdqa [rsp], xmm0
+    
+    xor eax, eax
+    crc32 rax, qword ptr [rsp]
+    crc32 rax, qword ptr [rsp + 8]
+    add rsp, 16
+    
+    ; Przetwórz pozostałe bajty (rsi zawiera liczbę)
+    test rsi, rsi
+    jz done
+    
+remaining_bytes:
+    crc32 eax, byte ptr [rdi]
+    inc rdi
+    dec rsi
+    jnz remaining_bytes
+    jmp done
 
-use_legacy:
-   
-    mov r8, rsi      
-    mov ecx, eax     
-    mov rdx, rdi     
+scalar_fallback:
+    ; Przetwarzaj 8 bajtów naraz
+    mov r9, rsi
+    shr r9, 3           ; r9 = liczba qwordów
+    jz scalar_bytes     ; jeśli brak qwordów, idź do bajtów
     
-    cmp r8, 0
-    jz fusion_ret
-    
-   
-    mov r9, r8
-    shr r9, 3
-    jz fusion_tail_bytes
-align 16
-fusion_qword_loop:
-    crc32 rax, qword ptr [rdx]
-    add rdx, 8
+    align 16
+scalar_qword_loop:
+    crc32 rax, qword ptr [rdi]
+    add rdi, 8
     dec r9
-    jnz fusion_qword_loop
-fusion_tail_bytes:
-    and r8, 7
-    jz fusion_ret
-fusion_tail_byte_loop:
-    crc32 eax, byte ptr [rdx]
-    inc rdx
-    dec r8
-    jnz fusion_tail_byte_loop
+    jnz scalar_qword_loop
 
-fusion_ret:
-    add rsp, 32
+scalar_bytes:
+    and rsi, 7          ; pozostałe bajty (0-7)
+    jz done
+scalar_byte_loop:
+    crc32 eax, byte ptr [rdi]
+    inc rdi
+    dec rsi
+    jnz scalar_byte_loop
+
+done:
     pop rsi
     pop rdi
     pop rbx
     ret
-
 AsmCrc32cUpdateFusion ENDP
+
+AsmCrc32cUpdateReport PROC FRAME
+    push rdi
+    .pushreg rdi
+    push rsi
+    .pushreg rsi
+    .endprolog
+    
+    mov eax, ecx
+    mov rdi, rdx
+    mov rsi, r8
+    
+    cmp rsi, 128
+    jb L_scalar_fallback
+    
+    mov r9, rsi
+    shr r9, 7
+    
+    align 16
+L_main_loop:
+    crc32 rax, qword ptr [rdi]
+    crc32 rax, qword ptr [rdi + 8]
+    crc32 rax, qword ptr [rdi + 16]
+    crc32 rax, qword ptr [rdi + 24]
+    crc32 rax, qword ptr [rdi + 32]
+    crc32 rax, qword ptr [rdi + 40]
+    crc32 rax, qword ptr [rdi + 48]
+    crc32 rax, qword ptr [rdi + 56]
+    crc32 rax, qword ptr [rdi + 64]
+    crc32 rax, qword ptr [rdi + 72]
+    crc32 rax, qword ptr [rdi + 80]
+    crc32 rax, qword ptr [rdi + 88]
+    crc32 rax, qword ptr [rdi + 96]
+    crc32 rax, qword ptr [rdi + 104]
+    crc32 rax, qword ptr [rdi + 112]
+    crc32 rax, qword ptr [rdi + 120]
+    
+    add rdi, 128
+    dec r9
+    jnz L_main_loop
+    
+    and rsi, 127
+    
+L_scalar_fallback:
+    mov r9, rsi
+    shr r9, 3
+    jz L_bytes
+    
+L_qword_loop:
+    crc32 rax, qword ptr [rdi]
+    add rdi, 8
+    dec r9
+    jnz L_qword_loop
+    
+L_bytes:
+    and rsi, 7
+    jz L_done
+    
+L_byte_loop:
+    crc32 eax, byte ptr [rdi]
+    inc rdi
+    dec rsi
+    jnz L_byte_loop
+    
+L_done:
+    pop rsi
+    pop rdi
+    ret
+AsmCrc32cUpdateReport ENDP
 
 END
